56b5cd5472d58de29de871c79bc78c854079766a
Shreeyasha Pandey <60794230+shree131@users.noreply.github.com>
2023-12-10 13:02:26 -0600

    Fetched data and updated movie and tv details pages

--- js/script.js
+++ js/script.js
@@ -54,10 +54,80 @@ const displayCard = (item, isMovie) => {
     document.querySelector(parentId).appendChild(cardDiv);
 };
 
+const displayMovieDetails = async () => {
+    const queryStr = window.location.search.slice(1).split('=');
+    const movieId = getQueryObject(queryStr).id;
+    const movie = await fetchAPIData(`movie/${movieId}`);
+    displayDetails(movie, true);
+};
+
+const displayTVDetails = async () => {
+    const queryStr = window.location.search.slice(1).split('=');
+    const seriesId = getQueryObject(queryStr).id;
+    const show = await fetchAPIData(`tv/${seriesId}`);
+    displayDetails(show, false);
+};
+
+const displayDetails = (item, isMovie) => {
+    const name = isMovie ? item.original_title : item.name;
+    const releaseInfo = isMovie ? `Release Date: ${item.release_date}` : `Aired Date: ${item.first_air_date}`;
+    const imgSrc = item.poster_path ? `https://image.tmdb.org/t/p/w500${item.poster_path}` : 'images/no-image.jpg';
+
+    const divTop = document.querySelector('.details-top');
+    const divBottom = document.querySelector('.details-bottom');
+    const divImg = document.createElement('div');
+    const divInfo = document.createElement('div');
+
+    divImg.innerHTML = `<img src="${imgSrc}"
+							class="card-img-top"
+							alt="${name}" />`;
+
+    divInfo.innerHTML = `<h2>${name}</h2>
+						<p><i class="fas fa-star text-primary"></i> ${item.vote_average.toFixed(1)} / 10</p>
+						<p class="text-muted">${releaseInfo}</p>
+						<p> ${item.overview} </p>
+						<h5>Genres</h5>
+						<ul class="list-group">
+							${item.genres.map(genre => `<li>${genre.name}</li>`).join('')}
+						</ul>
+						<a href="${item.homepage}" target="_blank" class="btn">Visit ${isMovie ? `Movie` : `Show`} Homepage</a>`;
+
+    divBottom.innerHTML = isMovie ? `<h2>Movie Info</h2>
+        <ul>
+        <li><span class="text-secondary">Budget:</span> $${formatCurrency(item.budget)}</li>
+        <li><span class="text-secondary">Revenue:</span> $${formatCurrency(item.revenue)}</li>
+        <li><span class="text-secondary">Runtime:</span> ${item.runtime} minutes</li>
+        <li><span class="text-secondary">Status:</span> ${item.status}</li>
+        </ul>
+        <h4>Production Companies</h4>
+        <div class="list-group">
+        ${item.production_companies.map(comp => `${comp.name}`).join(', ')}</div>` :
+
+        `<h2>Show Info</h2>
+        <ul>
+          <li><span class="text-secondary">Number Of Episodes:</span> ${item.number_of_episodes}</li>
+          <li><span class="text-secondary">Last Episode To Air:</span> ${item.last_episode_to_air.name} (S${item.last_episode_to_air.season_number}E${item.last_episode_to_air.episode_number})</li>
+          <li><span class="text-secondary">Status:</span> ${item.status}</li>
+        </ul>
+        <h4>Production Companies</h4>
+        <div class="list-group">${item.production_companies.map(comp => `${comp.name}`).join(', ')}</div>`;
+
+    divTop.appendChild(divImg);
+    divTop.appendChild(divInfo);
+};
+
+const getQueryObject = (queryStr) => {
+    let paramsObj = {};
+    for (let i = 0; i < queryStr.length; i += 2) {
+        paramsObj[queryStr[i]] = queryStr[i + 1];
+    }
+    return paramsObj;
+};
+
 // Fetch data from TMDB API
 const fetchAPIData = async (endpoint) => {
     const API_URL = 'https://api.themoviedb.org/3/';
-    const API_KEY = 'f7cad7e0325a6e76fff0ccfbee7268fc';
+    // const API_KEY = 'f7cad7e0325a6e76fff0ccfbee7268fc';
     //const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
 
     showSpinner();
@@ -84,6 +154,10 @@ const highlightActiveLink = () => {
     });
 };
 
+const formatCurrency = (amount) => {
+    return amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
+
 // Init App
 const init = () => {
     switch (state.currPage) {
@@ -98,9 +172,11 @@ const init = () => {
             break
         case '/movie-details.html':
             console.log('Movie details');
+            displayMovieDetails();
             break;
         case '/tv-details.html':
             console.log('TV details');
+            displayTVDetails();
             break;
         case '/search.html':
             console.log('Search Page');
